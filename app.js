b223c29ed84bdedc88faa90d4ac301f5fc883450
cstultz87 <cstultz@oswego.edu>
2019-02-03 22:52:30 -0600

    tasks

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
     // Remove task event
-    taskList.addEventListener('click', removeTask)
+    taskList.addEventListener('click', removeTask);
+    //Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 
 }
 
@@ -47,5 +51,35 @@ function addTask(e) {
 
 //Remove Task 
 function removeTask(e) {
-    console.log(e.target);
+    if(e.target.parentElement.classList.contains
+        ('delete-item')) {
+            if(confirm('Are You Sure?')) {
+                e.target.parentElement.parentElement.remove();
+            }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    //taskList.innerHTML = '';
+
+    // Faster ...
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+//Filter Tasks 
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+    (function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
